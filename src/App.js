ca3eb340a9b88facd6409ab6083e1da2c7eb3e17
R.Ganesh <ganesramesh110@gmail.com>
2016-12-22 14:33:07 +0530

    An JSX to Javascript Transpiler

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './App.css'
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      input:'add your JSX here',
+      output:'',
+      err:''
+    }
+  }
+  update(e){
+    let code = e.target.value;
+    try {
+      this.setState({
+        output:window.Babel
+                .transform(code,{presets:['es2015','react']})
+                .code,
+                err:''
+      })
+    } catch (err) {
+          this.setState({err:err.message})
+
+    }
+  }
+  render() {
+    return (
+      <div>
+        <header>Error:{this.state.err}</header>
+        <div className="container">
+          <textarea
+            onChange={this.update.bind(this)}
+            defaultValue ={this.state.input}/>
+          <pre>
+            {this.state.output}
+          </pre>
+
+        </div>
+        </div>
+    );
+  }
+}
+export default App
